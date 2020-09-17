4dc6920230023d0dcd5d47311285bcc9bcf0f970
Jacopo <jakonigro@gmail.com>
2020-09-17 15:23:21 +0200

    fine esercizio senza bonus

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
-$(document).ready(function() {
-	//Code
+// document.ready
+
+$(document).ready(function(){
+  // richiesta ajax
+
+  $.ajax({
+    'url': 'https://flynn.boolean.careers/exercises/api/array/music',
+    'method': 'GET',
+    'success': function(data) {
+      inserisciAlbum(data.response);
+    },
+    'error': function(request, state, errors) {
+      alert('Errore ' + errors);
+    }
+  });
+  // fine richiesta ajax
+
+  // funzione inserisciAlbum
+
+function inserisciAlbum(album) {
+for (var i = 0; i<album.length; i++) {
+var cd = album[i];
+var sorgente = $("#entry-template").html();
+var template = Handlebars.compile(sorgente);
+var domHtml = template(cd);
+$('.cds-container').append(domHtml);
+}
+}
 });
+// fine document.ready
